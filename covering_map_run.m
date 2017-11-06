b8863246b7196adf0b330ed77fd4c6988880ac39
Robert Mok <robertmok@eduroam-int-dhcp-97-87-237.ucl.ac.uk>
2017-11-06 12:25:38 +0000

    path

--- covering_map_run.m
+++ covering_map_run.m
@@ -1,8 +1,9 @@
 clear all;
 % close all;
 
-% wd='/Users/robert.mok/Dropbox/Grid_cell_model';
-wd='/Users/robertmok/Dropbox/Grid_cell_model';
+% wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
+wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
+
 cd(wd);
 codeDir = [wd '/code_gridCell'];
 saveDir = [wd '/data_gridCell'];
