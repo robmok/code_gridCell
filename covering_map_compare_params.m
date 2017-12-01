44441bccb44195e7c8c6b637f36ecf721af1b598
Robert Mok <robert.mok@love06.psychlangsci.ucl.ac.uk>
2017-12-01 16:20:49 +0000

    update

--- covering_map_compare_params.m
+++ covering_map_compare_params.m
@@ -1,7 +1,7 @@
 clear all;
 
-wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
-% wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
+% wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
+wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
 cd(wd);
 
 codeDir = [wd '/code_gridCell'];
@@ -24,7 +24,6 @@ dataPtsTest = [randsample(linspace(locRange(1),locRange(2),nSteps),nTrialsTest,'
 doPlot = 0; %plot - note if loading in many value, this will make too many plots, can crash
 
 gaussSmooth=1; %smooth maps by x value
-% dsFactor=5; %downsample the map so faster computing autocorr map
 
 %for computing density map
 nTrlsToUse = 10000; %
@@ -32,154 +31,141 @@ toTrlN     = nTrials; %-10000; %nTrials if from nTrlstoUse to end,
 fromTrlI   = toTrlN-nTrlsToUse+1;
 
 
-%define which parmeters to test (if not testing, keep at 1 value)
+%params to compare
+epsMuVals = [50, 75, 100]; %atm, lowest learning rate looks best
+% epsMuOrig1000 = 75; %50, 75, 100 - atm, lowest learning rate looks best
 
-%how to test several; or should test one at a time?
-%%%%%%%
-alphaVals = 2;
-%[0,2,4,6,8]; 
+alphaVals = [2, 5, 8];
 
 stochasticType = 1; %1, 2, 3; % stochasticVals=[1,2,3] - later
-
-cVals = ([.1/nTrials, .25/nTrials, .5/nTrials, 2/nTrials, 3/nTrials, 5/nTrials, 10/nTrials, 20/nTrials]); %.1 and stoch = towards middle
-% cVals = [2/nTrials, 3/nTrials, 5/nTrials, 10/nTrials];
-% cVals = [3/nTrials];
+cVals = ([.25/nTrials, .5/nTrials, 2/nTrials, 3/nTrials, 5/nTrials, 10/nTrials, 20/nTrials]); %.1 and stoch = towards middle % cVals = [20/nTrials];
 cVals = round(cVals.*1000000);
 
-%%%%%%%
-%set which one to test
-%%%%%%
-testVals = cVals; %alphaVals, cVals, stochasticVals
-nTestVals = length(testVals);
+nEps   = length(epsMuVals);
+nAlpha = length(alphaVals);
+nC     = length(cVals);
 
 nClus = 20; %20, 30
-colors = distinguishable_colors(nClus);
+
 nTrials = 40000;
 nIter = 10;
-
 %warpBox=0;
 
-clusMu=nan(nClus,2,nIter,length(testVals));
-sse=nan(1,nClus);
-tsse=nan(nIter,length(testVals));
-devAvgSSE=nan(nClus,nIter,length(testVals));
-
 spacing = linspace(locRange(1),locRange(2),locRange(2)+1);
-densityPlot = zeros(length(spacing),length(spacing),nIter,nTestVals);
-aCorrMap = nan(length(spacing)*2-1,length(spacing)*2-1,nIter,nTestVals);
+tsse=nan(nIter,nC);
+devAvgSSE=nan(nClus,nIter,nEps,nAlpha,nC);
+densityPlot = zeros(length(spacing),length(spacing),nIter,nEps,nAlpha,nC);
+aCorrMap = nan(length(spacing)*2-1,length(spacing)*2-1,nIter,nEps,nAlpha,nC);
 
-
-for iTestVal = 1:nTestVals
-%     iPlot=0; %for subplot below
-    
-    %set params, load in data
-    epsMuOrig1000 = 75; %75, 100
-    
-    %alpha
-    %     alpha10 = alphaVals(iTestVal);
-    alpha10 = alphaVals;
-
-    %stochastic parameters
-    c =cVals(iTestVal);
-    fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_stype%d_cVal%d_%diters*',nClus,nTrials,epsMuOrig1000,alpha10,stochasticType,c,nIter)];
-    
-    %if ran more than 1 set of iters, hv multiple files w diff end bit;load
-    f = dir(fname); filesToLoad = cell(1,length(f));
-    muAllTmp={};
-    nIterCount=0;
-    for iF = 1:length(f)
-%         filesToLoad{iF} = f(iF).name;
-%         fnames = [fnames filesToLoad{iF}];
-        load(f(iF).name);
-        muAllTmp{iF}=muAll;
-        nIterCount = [nIterCount, nIter+nIterCount(end)]; %count number of iters to index below when merging
-    end
-    %merge
-    clear muAll
-    for iF = 1:length(f)
-        muAll(:,:,:,nIterCount(iF)+1:nIterCount(iF+1)) = muAllTmp{iF};
-    end
-   
-    %calculate
-    for iterI = 1:size(muAll,4) %nIter
-        %compute density map
-        clus = round(muAll(:,:,fromTrlI:toTrlN,iterI));
-        densityPlotClus     = zeros(length(spacing),length(spacing),nClus);
-        densityPlotClusSmth = zeros(length(spacing),length(spacing),nClus);
-        for iClus=1:nClus
-            for iTrl=1:size(clus,3)
-                %                 densityPlot(clus(i,1,iTrl),clus(i,2,iTrl),iterI,iTestVal)=densityPlot(clus(i,1,iTrl),clus(i,2,iTrl),iterI,iTestVal)+1; % works, but better way / faster to vectorise?
-                densityPlotClus(clus(iClus,1,iTrl),clus(iClus,2,iTrl),iClus) = densityPlotClus(clus(iClus,1,iTrl),clus(iClus,2,iTrl),iClus)+1;
-            end
-            %find peaks
-            densityPlotClusSmth(:,:,iClus)=imgaussfilt(densityPlotClus(:,:,iClus),gaussSmooth);
-            [peakX, peakY] = find(densityPlotClusSmth(:,:,iClus)==max(max((densityPlotClusSmth(:,:,iClus)))));
-            clusMu(iClus,:,iterI) = [peakX, peakY];
+for iEps = 1:nEps
+    for iAlpha = 1:nAlpha
+        
+        for iC = 1:nC
+            %     iPlot=0; %for subplot below
             
-            %make combined (grid cell) plot
-            densityPlot(:,:,iterI,iTestVal) = sum(densityPlotClus,3);
-            densityPlot(:,:,iterI,iTestVal) = imgaussfilt(densityPlot(:,:,iterI,iTestVal),gaussSmooth); %smooth
+            
+            %set params, load in data
+            
+            %learning rate
+            epsMuOrig1000 = epsMuVals(iEps);
+            
+            %alpha - momentum parameter
+            alpha10 = alphaVals(iAlpha);
+            
+            %stochastic parameters
+            c =cVals(iC);
+            
+            fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_stype%d_cVal%d_%diters*',nClus,nTrials,epsMuOrig1000,alpha10,stochasticType,c,nIter)];
+            
+            %if ran more than 1 set of iters, hv multiple files w diff end bit;load
+            f = dir(fname); filesToLoad = cell(1,length(f));
+            muAllTmp={};
+            nIterCount=0;
+            for iF = 1:length(f)
+                %         filesToLoad{iF} = f(iF).name;
+                %         fnames = [fnames filesToLoad{iF}];
+                load(f(iF).name);
+                muAllTmp{iF}=muAll;
+                nIterCount = [nIterCount, nIter+nIterCount(end)]; %count number of iters to index below when merging
+            end
+            %merge
+            clear muAll
+            for iF = 1:length(f)
+                muAll(:,:,:,nIterCount(iF)+1:nIterCount(iF+1)) = muAllTmp{iF};
+            end
+            
+            %calculate
+            clusMu=nan(nClus,2,nIter);
+            for iterI = 1:size(muAll,4) %nIter
+                %compute density map
+                clus = round(muAll(:,:,fromTrlI:toTrlN,iterI));
+                densityPlotClus     = zeros(length(spacing),length(spacing),nClus);
+                densityPlotClusSmth = zeros(length(spacing),length(spacing),nClus);
+                for iClus=1:nClus
+                    for iTrl=1:size(clus,3)
+                        %                 densityPlot(clus(i,1,iTrl),clus(i,2,iTrl),iterI,iTestVal)=densityPlot(clus(i,1,iTrl),clus(i,2,iTrl),iterI,iTestVal)+1; % works, but better way / faster to vectorise?
+                        densityPlotClus(clus(iClus,1,iTrl),clus(iClus,2,iTrl),iClus) = densityPlotClus(clus(iClus,1,iTrl),clus(iClus,2,iTrl),iClus)+1;
+                    end
+                    %find peaks
+                    densityPlotClusSmth(:,:,iClus)=imgaussfilt(densityPlotClus(:,:,iClus),gaussSmooth);
+                    [peakX, peakY] = find(densityPlotClusSmth(:,:,iClus)==max(max((densityPlotClusSmth(:,:,iClus)))));
+                    clusMu(iClus,:,iterI) = [peakX, peakY];
+                    
+                    %make combined (grid cell) plot
+                    densityPlot(:,:,iterI,iEps,iAlpha,iC) = sum(densityPlotClus,3);
+                    densityPlot(:,:,iterI,iEps,iAlpha,iC) = imgaussfilt(densityPlot(:,:,iterI,iEps,iAlpha,iC),gaussSmooth); %smooth
+                end
+                                
+                % compute sse with respect to all test data points
+                distTrl=[];
+                sse=nan(1,nClus);
+                for iClus = 1:size(clusMu,1)
+                    distTrl(:,iClus)=sum([clusMu(iClus,1,iterI)-dataPtsTest(:,1), clusMu(iClus,2,iterI)-dataPtsTest(:,2)].^2,2);
+                end
+                [indValsTrl, indTmp]=min(distTrl,[],2); % find which clusters are points closest to
+                for iClus = 1:size(clusMu,1)
+                    sse(iClus)=sum(sum([clusMu(iClus,1,iterI)-dataPtsTest(indTmp==iClus,1), clusMu(iClus,2,iterI)-dataPtsTest(indTmp==iClus,2)].^2,2)); %distance from each cluster from training set to datapoints closest to that cluster
+                end
+                tsse(iterI,iEps,iAlpha,iC)=sum(sse);
+                
+                %compute 'spreaded-ness' - variance of SE across clusters is a measure
+                %of this, assuing uniform data points
+                devAvgSSE            = sse-mean(sse);
+                stdAcrossClus(iterI,iEps,iAlpha,iC) = std(devAvgSSE);
+                varAcrossClus(iterI,iEps,iAlpha,iC) = var(devAvgSSE);
+                
+                %compute autocorrelation map
+                aCorrMap(:,:,iterI,iEps,iAlpha,iC) = ndautoCORR(densityPlot(:,:,iterI,iEps,iAlpha,iC));
+                
+                %plot
+                if doPlot
+                    figure;
+                    subplot(2,2,1);
+                    imagesc(densityPlot(:,:,iterI,iEps,iAlpha,iC));
+                    subplot(2,2,2);
+                end
+                [g,gdataA] = gridSCORE(aCorrMap(:,:,iterI,iEps,iAlpha,iC),'allen',doPlot);
+                if doPlot, subplot(2,2,3); end
+                [g,gdataW] = gridSCORE(aCorrMap(:,:,iterI,iEps,iAlpha,iC),'wills',doPlot);
+                
+                gA_gA(iterI,iEps,iAlpha,iC)   = gdataA.g_score;
+                gA_oA(iterI,iEps,iAlpha,iC)   = gdataA.orientation;
+                gA_wavA(iterI,iEps,iAlpha,iC) = gdataA.wavelength;
+                gA_radA(iterI,iEps,iAlpha,iC) = gdataA.radius;
+                
+                gA_gW(iterI,iEps,iAlpha,iC)   = gdataW.g_score;
+                gA_oW(iterI,iEps,iAlpha,iC)   = gdataW.orientation;
+                gA_wavW(iterI,iEps,iAlpha,iC) = gdataW.wavelength;
+                gA_radW(iterI,iEps,iAlpha,iC) = gdataW.radius;
+            end
+            gA{iEps,iAlpha,iC}=gdataA;
+            gW{iEps,iAlpha,iC}=gdataW;
         end
-        
-        
-        
-        % compute sse with respect to all test data points
-        distTrl=[];
-        for iClus = 1:size(clusMu,1)
-            distTrl(:,iClus)=sum([clusMu(iClus,1,iterI)-dataPtsTest(:,1), clusMu(iClus,2,iterI)-dataPtsTest(:,2)].^2,2);
-        end
-        [indValsTrl, indTmp]=min(distTrl,[],2); % find which clusters are points closest to
-        for iClus = 1:size(clusMu,1)
-            sse(iClus)=sum(sum([clusMu(iClus,1,iterI)-dataPtsTest(indTmp==iClus,1), clusMu(iClus,2,iterI)-dataPtsTest(indTmp==iClus,2)].^2,2)); %distance from each cluster from training set to datapoints closest to that cluster
-        end
-        tsse(iterI,iTestVal)=sum(sse);
-        
-        %compute 'spreaded-ness' - variance of SE across clusters is a measure
-        %of this, assuing uniform data points
-        devAvgSSE            = sse-mean(sse);
-        stdAcrossClus(iterI,iTestVal) = std(devAvgSSE);
-        varAcrossClus(iterI,iTestVal) = var(devAvgSSE);
-        
-        %compute autocorrelation map
-        
-        %downsample
-%         dPlotTmp=densityPlot(:,:,iterI,iTestVal);
-%         ind=1:dsFactor:size(dPlotTmp,1);
-%         clear dPlotTmpDS
-%         for iRow = 1:size(dPlotTmp,1)/dsFactor
-%             for iCol = 1:size(dPlotTmp,1)/dsFactor
-%                 dPlotTmpDS(iCol,iRow) = dPlotTmp(ind(iRow),ind(iCol));
-%             end
-%         end
-%         densityPlotDS(:,:,iterI,iTestVal) =imgaussfilt(dPlotTmpDS,gaussSmooth);
-
-        aCorrMap(:,:,iterI,iTestVal) = ndautoCORR(densityPlot(:,:,iterI,iTestVal));
-        
-        %plot
-        if doPlot
-            figure;
-            subplot(2,2,1);
-            imagesc(densityPlot(:,:,iterI,iTestVal));
-            subplot(2,2,2);
-        end
-        [g,gdataA] = gridSCORE(aCorrMap(:,:,iterI,iTestVal),'allen',doPlot);
-        if doPlot, subplot(2,2,3); end
-        [g,gdataW] = gridSCORE(aCorrMap(:,:,iterI,iTestVal),'wills',doPlot);
-        
-        
-        gA_g(iterI,iTestVal)   = gdataA.g_score;
-        gA_o(iterI,iTestVal)   = gdataA.orientation;
-        gA_wav(iterI,iTestVal) = gdataA.wavelength;
-        gA_rad(iterI,iTestVal) = gdataA.radius;
-        
     end
-    gA{iTestVal}=gdataA;
-    gW{iTestVal}=gdataW;
 end
 
-
 % % plot
-% for iTestVal = 1:nTestVals
+% for iTestVal = 1:nCvals
 %     figure;
 %     for iterI = 1:nIter
 %         subplot(2,2,1);
@@ -195,45 +181,118 @@ end
 % however can recover it by using the same loops when loading in the param
 % names? e.g. make a variable with the param names, then plot them
 
-barpos = .25:.5:.5*nTestVals;
 barw   = .25;
+colgrey = [.25 .25 .25]
 
-%compare with bar plots, etc.
+gridMeasure = 'grid'; % grid, angle (orientation), rad (radius), wav (wavelength)
+gridMsrType = 'a'; % a/w - allen or willis
 
-dat=gA_g;
-
-figure; hold on;
-mu=mean(dat,1);
-sm=std(dat)./sqrt(size(dat,1));
-ci=sm.*tinv(.025,size(dat,1)-1); %compute conf intervals
-for i=1:length(mu)
-    h(i) = bar(barpos(i),mu(i),barw);
+switch gridMsrType
+    case 'a'
+        gA_g = gA_gA;
+        gA_o = gA_oA;
+        gA_rad = gA_radA;
+        gA_wav = gA_wavA;
+    case 'w'
+        gA_g = gA_gW;
+        gA_o = gA_oW;
+        gA_rad = gA_radW;
+        gA_wav = gA_wavW;        
 end
-% he = errorbar(barpos,mu,sm,-sm,'k.');
-he = errorbar(barpos,mu,ci,'k.');
-xlim([barpos(1)-.5, barpos(end)+.5]);
-
-% [h p c s] = ttest(gA_g(:,2),gA_g(:,4))
 
 
+% %remove nans - this is a bit hacky now...
+% if any(isnan(gA_g(1,:)))
+%     ind=isnan(dat(1,:)); gA_g(:,ind)=[]; gA_o(:,ind)=[];gA_rad(:,ind)=[];gA_wav(:,ind)=[]; barpos(ind)=[]; colors(ind,:)=[];
+% end
+% if length(barpos)==length(ind); barpos(ind)=[]; colors(ind,:)=[]; end %this is reset at top..
 
 
-dat=gA_o;
-
-figure; hold on;
-mu=mean(dat,1);
-sm=std(dat)./sqrt(size(dat,1));
-ci=sm.*tinv(.025,size(dat,1)-1);
-for i=1:length(mu)
-    h(i) = bar(barpos(i),mu(i),barw);
+%%
+% compare alpha and c for each learning rate
+% for iAlpha = 1:nAlpha %comparing cVals
+%     figure; hold on;    
+%     for iC=1:nC
+%         subplot(1,7,iC);
+for iC=1:nC %comparing alpha vals
+    figure; hold on;
+    for iAlpha = 1:nAlpha
+        subplot(1,3,iAlpha);
+        
+        switch gridMeasure
+            case 'grid'
+                datTmp=gA_g;
+            case 'angle'
+                datTmp=gA_o;
+            case 'rad'
+                datTmp=gA_rad;
+            case 'wav'
+                datTmp=gA_wav;
+        end
+        dat=datTmp(:,:,iAlpha,iC);
+        barpos = .25:.5:.5*size(dat,2);
+        colors = distinguishable_colors(size(dat,2));
+        
+        mu=mean(dat,1);
+        sm=std(dat)./sqrt(size(dat,1));
+        ci=sm.*tinv(.025,size(dat,1)-1); %compute conf intervals
+        % figure; hold on;
+        plotSpread(dat,'xValues',barpos,'distributionColors',colors);
+        errorbar(barpos,mu,ci,'Color',colgrey,'LineStyle','None','LineWidth',1);
+        scatter(barpos,mu,750,colors,'.');
+        xlim([barpos(1)-.5, barpos(end)+.5]);
+        if strcmp(gridMeasure,'grid'), ylim([-.5, 1]); end
+        %         title(sprintf('%s - eps=%d, alpha=%d, c=%d',gridMeasure,epsMuVals(iEps),alphaVals(iAlpha),cVals(iC)))
+        title(sprintf('%s - alpha=%d, c=%d',gridMeasure,alphaVals(iAlpha),cVals(iC)))
+    end
 end
-% he = errorbar(barpos,mu,sm,-sm,'k.');
-he = errorbar(barpos,mu,ci,'k.');
-xlim([barpos(1)-.5, barpos(end)+.5]);
 
 
 
-%univariate scatter?
+for iEps=1:nEps
+    figure; hold on;
+    for iAlpha = 1:nAlpha
+        subplot(1,3,iAlpha);
+        
+        switch gridMeasure
+            case 'grid'
+                datTmp=gA_g;
+            case 'angle'
+                datTmp=gA_o;
+            case 'rad'
+                datTmp=gA_rad;
+            case 'wav'
+                datTmp=gA_wav;
+        end
+        dat=squeeze(datTmp(:,iEps,iAlpha,:));
+        barpos = .25:.5:.5*size(dat,2);
+        colors = distinguishable_colors(size(dat,2));
+        
+        mu=mean(dat,1);
+        sm=std(dat)./sqrt(size(dat,1));
+        ci=sm.*tinv(.025,size(dat,1)-1); %compute conf intervals
+        % figure; hold on;
+        plotSpread(dat,'xValues',barpos,'distributionColors',colors);
+        errorbar(barpos,mu,ci,'Color',colgrey,'LineStyle','None','LineWidth',1);
+        scatter(barpos,mu,750,colors,'.');
+        xlim([barpos(1)-.5, barpos(end)+.5]);
+        if strcmp(gridMeasure,'grid'), ylim([-.5, 1]); end
+        title(sprintf('%s - eps=%d, alpha=%d',gridMeasure,epsMuVals(iEps),alphaVals(iAlpha)))
+    end
+end
+
+%%%%%%%%%%%%
+% ++ more ways of plotting / comparing? maybe add the mean gridness score
+% to the plot?
+%%%%%%%%%%%%
+
+
+
+
+
+
+
+
 
 
 
@@ -268,7 +327,7 @@ xlim([barpos(1)-.5, barpos(end)+.5]);
 % for iterI=1:nIter
 %     % figure('units','normalized','outerposition',[0 0 1 1]); hold on; iPlot=0;
 %     figure; hold on; iPlot=0;
-%     for iTestVal = 1:length(testVals)
+%     for iTestVal = 1:nC
 %         iPlot = iPlot+1;
 %         subplot(2,2,iPlot);
 %         scatter(clusMu(:,1,iterI,iTestVal),clusMu(:,2,iterI,iTestVal),20e+2,colors,'.')
--- covering_map_run.m
+++ covering_map_run.m
@@ -1,8 +1,8 @@
 clear all;
 % close all;
 
-% wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
-wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
+wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
+% wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
 % wd='/home/robmok/Documents/Grid_cell_model'; %on love01
 
 cd(wd);
@@ -20,14 +20,12 @@ locRange = [0, nSteps-1]; %[-1, 1]; % from locRange(1) to locRange(2)
 stepSize=diff(linspace(locRange(1),locRange(2),nSteps)); stepSize=stepSize(1); %smallest diff between locs
 
 % parameters
-epsMuOrig=.075;% %learning rate / starting learning rate %.075
-% epsMuOrig=.1;
-% epsMuOrig=.05;
+epsMuVals=[.05, .075, .1];% %learning rate / starting learning rate 
 % epsMuOrig=.125; %could do this as well
 % epsMuOrig = .01;
 
 % for saving simulations - multiple by values to save the files with params
-epsMuOrig1000=epsMuOrig*1000;
+% epsMuOrig1000=epsMuOrig*1000;
 % deltaEpsMu100 = deltaEpsMu*100;
 
 %define box / environement - random points in a box
@@ -40,8 +38,7 @@ warpType = 'sq2rect';
 
 %mometum-like adaptive learning rate - define alpha (higher = weight
 %previous update (direction and magnitude) more; 0 = don't weight previous at all)
-% alphaVals = [.2, .5, .8];
-alphaVals = .2 ; %.2 .5 .8
+alphaVals = [.2, .5, .8];
 
 sTypes = 1;%:3; %0, 1 ,2, 3
 % 0. none
@@ -52,9 +49,8 @@ sTypes = 1;%:3; %0, 1 ,2, 3
 % 3. constant stochasticity - keep very low
 
 %  larger c = less stochastic over trials (becomes det quite early on); smaller c = more stochastic over trials (still a bit stochastic by the end)
-cVals = [2/nTrials, 3/nTrials, 5/nTrials, 10/nTrials]; %half1
-% cVals = [.1/nTrials, .25/nTrials, .5/nTrials, 20/nTrials]; %half2
-cVals = 10/nTrials;
+cVals = [.25/nTrials, .5/nTrials, 2/nTrials, 3/nTrials, 5/nTrials, 10/nTrials, 20/nTrials]; %removed .1/nTrials, too stochastic
+% cVals = 20/nTrials;
 
 % % Create / load in saved test data
 % trials = [randsample(linspace(-locRange,locRange,101),nTrials,'true'); randsample(linspace(-locRange,locRange,101),nTrials,'true')]'; % random points in a box
@@ -74,38 +70,42 @@ cVals = 10/nTrials;
 % save([saveDir '/randTrialsBox_40k'],'trials');
 
 %%
-saveDat=0; %save simulations
+saveDat=1; %save simulations
 
 load([saveDir '/randTrialsBox_40k']); %load in same data with same trial sequence so same for each sim
 
-nIter=1; %how many iterations (starting points)
+nIter=10; %how many iterations (starting points)
 
 tic
-for iClus2run = 1:length(clus2run) %nClus conditions to run
-    nClus = clus2run(iClus2run);
-    for iStype = 1:length(sTypes)
-        stochasticType = sTypes(iStype);
-        for iStochastic = 1:length(cVals) %
-            c = cVals(iStochastic);
-            c1m=round(c.*1000000); % for saving file name
-            fprintf('Running cVal %d\n',c1m)
-            if ~stochasticType, c=0; end
-            for iAlpha  = 1:length(alphaVals) %
-                alpha = alphaVals(iAlpha);
-                alpha10 = alpha*10; %for saving simulations
-                fprintf('Running alphaVal %0.2f\n',alpha);
-                tic
-                [muAll,cParams] = covering_map_sim(nClus,locRange,box,warpType,epsMuOrig,nTrials,nIter,warpBox,alpha,trials,stochasticType,c);
-%                 muAll = covering_map_sim_neigh(nClus,locRange,box,warpType,epsMuOrig,nTrials,nIter,warpBox,alpha,trials,stochasticType,c);
-                timeTaken=toc;
-                if saveDat
-                    %                 fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_%diters',nClus,nTrials,epsMuOrig1000,alpha10,nIter)];
-                    fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_stype%d_cVal%d_%diters',nClus,nTrials,epsMuOrig1000,alpha10,stochasticType,c1m,nIter)];
-                    if warpBox
-                        fname = [fname '_warpBox'];
+for iEps = 1:length(epsMuVals)
+    epsMuOrig=epsMuVals(iEps);
+    epsMuOrig1000=epsMuOrig*1000; %for saving
+    for iClus2run = 1:length(clus2run) %nClus conditions to run
+        nClus = clus2run(iClus2run);
+        for iStype = 1:length(sTypes)
+            stochasticType = sTypes(iStype);
+            for iStochastic = 1:length(cVals) %
+                c = cVals(iStochastic);
+                c1m=round(c.*1000000); % for saving file name
+                fprintf('Running cVal %d\n',c1m)
+                if ~stochasticType, c=0; end
+                for iAlpha  = 1:length(alphaVals) %
+                    alpha = alphaVals(iAlpha);
+                    alpha10 = alpha*10; %for saving simulations
+                    fprintf('Running alphaVal %0.2f\n',alpha);
+                    tic
+                    [muAll,cParams] = covering_map_sim(nClus,locRange,box,warpType,epsMuOrig,nTrials,nIter,warpBox,alpha,trials,stochasticType,c);
+                    %                 muAll = covering_map_sim_neigh(nClus,locRange,box,warpType,epsMuOrig,nTrials,nIter,warpBox,alpha,trials,stochasticType,c);
+                    timeTaken=toc;
+                    if saveDat
+                        %                 fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_%diters',nClus,nTrials,epsMuOrig1000,alpha10,nIter)];
+                        fname = [saveDir, sprintf('/covering_map_dat_%dclus_%dtrls_eps%d_alpha%d_stype%d_cVal%d_%diters',nClus,nTrials,epsMuOrig1000,alpha10,stochasticType,c1m,nIter)];
+                        if warpBox
+                            fname = [fname '_warpBox'];
+                        end
+                        cTime=datestr(now,'HHMMSS'); fname = sprintf([fname '_%s'],cTime);
+                        save(fname,'muAll','nIter','cParams','timeTaken');
                     end
-                    cTime=datestr(now,'HHMMSS'); fname = sprintf([fname '_%s'],cTime);
-                    save(fname,'muAll','nIter','cParams','timeTaken');
                 end
             end
         end
