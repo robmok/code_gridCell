4c8dc8b328e7104b41148ba9c96fc80b71283f41
Rob Mok <rob1031@gmail.com>
2018-02-22 15:59:50 +0000

    update

--- covering_map_batch_run.m
+++ covering_map_batch_run.m
@@ -5,7 +5,7 @@ clear all;
 
 wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
 % wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
-% wd='/home/robmok/Documents/Grid_cell_model'; %on love01
+wd='/home/robmok/Documents/Grid_cell_model'; %on love01
 
 cd(wd);
 codeDir = [wd '/code_gridCell'];
@@ -25,7 +25,7 @@ clus2run = 20; %20, 30
 % clus2run = 18:2:24; %running 18:2:30; later could run 10:2:16
 % clus2run = 26:2:30;
 clus2run = 18;
-% clus2run = 26;
+clus2run = 26;
 
 % nTrials = 5000000; %how many locations in the box / trials 
 nTrials = 2500000; 
@@ -36,7 +36,7 @@ fixBatchSize = 1; %fixed, or batchSize depends on mean updates per cluster
 % 13, 25, 83, 125, 167, 250, 333, 500, 1000, 2000
 if fixBatchSize
     nBatches = [1250, 2500, 5000, 7500, 10000, 15000, 20000];
-%     nBatches = [30000, 100000, 200000, 500000];
+    nBatches = [30000, 100000, 200000, 500000];
 %     nBatches = [2500, 1250];
 %     nBatches = fliplr([20000, 30000, 100000, 200000, 500000]);
 %     nBatches = 500000;
