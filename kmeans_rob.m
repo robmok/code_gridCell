43c1c962f8eec730aaf3fa03ca9ebafddb0ddb09
Robert Mok <robert.mok@love06.psychlangsci.ucl.ac.uk>
2018-03-05 22:03:07 +0000

    update

--- kmeans_rob.m
+++ kmeans_rob.m
@@ -4,6 +4,8 @@ clear all;
 
 % wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
 wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
+wd='/home/robmok/Documents/Grid_cell_model'; %on love01
+
 cd(wd);
 
 codeDir = [wd '/code_gridCell'];
@@ -11,22 +13,26 @@ saveDir = [wd '/data_gridCell'];
 addpath(codeDir); addpath(saveDir);
 addpath(genpath([codeDir '/gridSCORE_packed']));
 
-kVals = 10:13; 
+kVals = 3:17; 
+% kVals = 18:25; 
+% kVals = 26:30; 
+% kVals = 3;
 nKvals = length(kVals);
 
-saveDat = 0;
+saveDat = 1;
 
 doXval        = 1;  %do (and save) crossvalidation
 nXvalDataSets = 20; %if do xVal, specify how many datasets to generate
 
-dat = 'randUnique'; %'rand' points in a box, randUnique (all unique points in box), or 'cat'
+dat = 'circ'; %'rand' points in a box, randUnique (all unique points in box), or 'cat'
 nKmeans = 1000;  % run k means n times %1000
 nPoints = 10000;  %how many locations/datapoints - not used for 'randUnique', though used for xVal
 
 
 %%%%
 locRange  = [0, 49];%.9; from -locRange to locRange
-spacing   = linspace(locRange(1),locRange(2),locRange(2)+1); 
+spacing   = linspace(locRange(1),locRange(2),locRange(2)+1);
+nSteps = length(spacing);
 gaussSmooth=1; % smoothing for density plot / autocorrelogram 
 
 % % normal k means / 'cat learning'
@@ -41,9 +47,26 @@ switch dat
         % does it matter how many points there are if all the same points? e.g.
         % same if just have each trialsUnique twice/x10? - i think not
         % dataPts = repmat(dataPts,50,1);
-    case 'rand'
+    case 'square'
         %uniformly sample the box
         dataPts = [randsample(linspace(locRange(1),locRange(2),50),nPoints,'true'); randsample(linspace(locRange(1),locRange(2),50),nPoints,'true')]';
+    case 'circ'
+        % Create logical image of a circle
+        imageSizeX = nSteps;
+        [columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeX);
+        centerX = nSteps/2; centerY = nSteps/2;
+        radius = nSteps/2-1;
+        circIm = (rowsInImage - centerY).^2 ...
+            + (columnsInImage - centerX).^2 <= radius.^2;
+        circPts=[]; % find circle points in XY coords
+        for iX=1:length(circIm)
+            yVals = find(circIm(iX,:));
+            circPts = [circPts; ones(length(yVals),1)*iX, yVals'];
+        end
+        trialInd=randi(length(circPts),nPoints,1);
+        dataPts=circPts(trialInd,:);
+        trialIndTest = randi(length(circPts),nPoints,1);
+        dataPts  = circPts(trialIndTest,:);
     case 'cat'
         % draw points from 2 categories (gaussian) from a 2D feature space
         nPointsCat = floor(nPoints/nCats); % points to sample
@@ -82,15 +105,16 @@ for kMeansIter=1:nKmeans
     mu   = nan(nK,2,nUpdSteps+1);
     densityPlotClus = zeros(length(spacing),length(spacing),nK);
     for i = 1:nK
-        switch dat
-            case 'rand'
-                mu(:,:,1) = kmplusInit(dataPts,nK); %k means ++ initiatialization
-            case 'randUnique'
-                mu(:,:,1) = kmplusInit(dataPts,nK); %k means ++ initiatialization
-            case 'cat'
-                mu(i,:,1) = dataPts(randi(length(dataPts)),:);   %intiate each cluster with one data point - Forgy method
-%                 mu(i,:,1) = round(locRange(1) + locRange(2).*rand(1,2));  %initiate clusters with a random point in the box
-        end
+%         switch dat
+%             case 'square'
+%                 mu(:,:,1) = kmplusInit(dataPts,nK); %k means ++ initiatialization
+%             case 'randUnique'
+%                 mu(:,:,1) = kmplusInit(dataPts,nK); %k means ++ initiatialization
+%             case 'cat'
+%                 mu(i,:,1) = dataPts(randi(length(dataPts)),:);   %intiate each cluster with one data point - Forgy method
+% %                 mu(i,:,1) = round(locRange(1) + locRange(2).*rand(1,2));  %initiate clusters with a random point in the box
+%         end
+        mu(:,:,1) = kmplusInit(dataPts,nK); %k means ++ initiatialization
     end
     
     %run kmeans
@@ -99,7 +123,7 @@ for kMeansIter=1:nKmeans
     tsseAll(kMeansIter)   = tsse;
 
     %compute gridness
-    if strcmp(dat,'rand') || strcmp(dat,'randUnique') %if cat learning, no need to compute gridness
+    if ~strcmp(dat,'cat') %if cat learning, no need to compute gridness
         for iClus=1:nK
             clusTmp  = squeeze(round(muAll(iClus,:,kMeansIter)))';
             for iTrlUpd=1:size(clusTmp,2)
@@ -127,12 +151,13 @@ toc
 end
 
 if saveDat
-    switch dat
-        case 'randUnique'
-            fname = [saveDir, sprintf('/kmeans_nK_%d-%d_uniquePts_%diters',kVals(1),kVals(end),nKmeans)];
-        case 'rand'
-            fname = [saveDir, sprintf('/kmeans_nK_%d-%d_randPts_%diters',kVals(1),kVals(end),nKmeans)];
-    end
+%     switch dat
+%         case 'randUnique'
+%             fname = [saveDir, sprintf('/kmeans_nK_%d-%d_uniquePts_%diters',kVals(1),kVals(end),nKmeans)];
+%         case 'rand'
+%             fname = [saveDir, sprintf('/kmeans_nK_%d-%d_randPts_%diters',kVals(1),kVals(end),nKmeans)];
+%     end
+    fname = [saveDir, sprintf('/kmeans_nK_%d-%d_%s_nPoints%d_%diters',kVals(1),kVals(end),dat,nPoints,nKmeans)];
     save(fname,'muAllkVals','tssekVals', 'gA','gW','densityPlotCentres','indSSE1','indSSE2','kVals')
 end
 
@@ -145,10 +170,28 @@ tsseXval = nan(nKmeans,nXvalDataSets,nKvals);
 for iDataSets = 1:nXvalDataSets
     fprintf('xVal dataset %d \n',iDataSets);
     switch dat
-        case 'rand' % draw random points in a box (uniform distribution)
+        case 'square' % draw random points in a box (uniform distribution)
             dataPtsTest = [randsample(linspace(locRange(1),locRange(2),50),nPoints,'true'); randsample(linspace(locRange(1),locRange(2),50),nPoints,'true')]'; % random points in a box
         case 'randUnique' % draw random points in a box (uniform distribution)
             dataPtsTest = [randsample(linspace(locRange(1),locRange(2),50),nPoints,'true'); randsample(linspace(locRange(1),locRange(2),50),nPoints,'true')]'; % random points in a box
+        case 'circ'
+            % Create logical image of a circle
+            imageSizeX = nSteps;
+            [columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeX);
+            centerX = nSteps/2; centerY = nSteps/2;
+            radius = nSteps/2-1;
+            circIm = (rowsInImage - centerY).^2 ...
+                + (columnsInImage - centerX).^2 <= radius.^2;
+            circPts=[]; % find circle points in XY coords
+            for iX=1:length(circIm)
+                yVals = find(circIm(iX,:));
+                circPts = [circPts; ones(length(yVals),1)*iX, yVals'];
+            end
+            trialInd=randi(length(circPts),nPoints,1);
+            trialIndTest     = randi(length(circPts),nPoints,1);
+            dataPtsTest      = circPts(trialIndTest,:);
+            trialIndTest = randi(length(circPts),nPoints,1);
+            dataPtsTest  = circPts(trialIndTest,:);
         case 'cat' % points from clusters of 2D gaussians
             nPointsCat = floor(nPoints/nCats); % points to sample
             for iCat = 1:nCats
@@ -186,12 +229,14 @@ for iDataSets = 1:nXvalDataSets
 end
 
 if saveDat
-    switch dat
-        case 'randUnique'
-            fname = [saveDir, sprintf('/kmeans_nK_%d-%d_uniquePts_xVal_%ddatasets_%diters',kVals(1),kVals(end),nXvalDataSets,nKmeans)];
-        case 'rand'
-            fname = [saveDir, sprintf('/kmeans_nK_%d-%d_randPts_xVal_%ddatasets_%diters',kVals(1),kVals(end),nXvalDataSets,nKmeans)];
-    end
+%     switch dat
+%         case 'randUnique'
+%             fname = [saveDir, sprintf('/kmeans_nK_%d-%d_uniquePts_xVal_%ddatasets_%diters',kVals(1),kVals(end),nXvalDataSets,nKmeans)];
+%         case 'rand'
+%             fname = [saveDir, sprintf('/kmeans_nK_%d-%d_randPts_xVal_%ddatasets_%diters',kVals(1),kVals(end),nXvalDataSets,nKmeans)];
+%     end
+    fname = [saveDir, sprintf('/kmeans_nK_%d-%d_%s_nPoints%d_xVal_%ddatasets_%diters',kVals(1),kVals(end),dat,nPoints,nXvalDataSets,nKmeans)];
+
     save(fname,'tsseXval','kVals')
 end
 end
