c5065dcd9954794a1df490bb3dcf7e7f33be1c6b
Robert Mok <robert.mok@love06.psychlangsci.ucl.ac.uk>
2018-03-06 12:24:02 +0000

    update

--- kmeans_rob.m
+++ kmeans_rob.m
@@ -4,7 +4,7 @@ clear all;
 
 % wd='/Users/robertmok/Documents/Postdoc_ucl/Grid_cell_model';
 wd='/Users/robert.mok/Documents/Postdoc_ucl/Grid_cell_model';
-% wd='/home/robmok/Documents/Grid_cell_model'; %on love01
+wd='/home/robmok/Documents/Grid_cell_model'; %on love01
 
 cd(wd);
 
@@ -13,15 +13,11 @@ saveDir = [wd '/data_gridCell'];
 addpath(codeDir); addpath(saveDir);
 addpath(genpath([codeDir '/gridSCORE_packed']));
 
-kVals = 3:11; 
-kVals = 12:17; 
-% kVals = 18:25; 
-% kVals = 26:30; 
-<<<<<<< HEAD
-% kVals = 10;
-=======
+% kVals = 3:11; 
+% kVals = 12:17; 
+kVals = 18:25; 
+kVals = 26:30; 
 % kVals = 15;
->>>>>>> b099964ae8053adf9cffefa09c0a001bafbf8338
 nKvals = length(kVals);
 
 saveDat = 1;
